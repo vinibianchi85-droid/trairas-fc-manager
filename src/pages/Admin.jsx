7a948820607df5892537d52d5b87f4d6211799fe
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 13:30:36 -0300

    Create Admin.jsx

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,19 @@
+export default function Admin() {
+  return (
+    <>
+      <div className="card">
+        <h2>⚙️ Administração</h2>
+
+        <button>Cadastrar jogo</button>
+
+        <br /><br />
+
+        <button>Cadastrar jogador</button>
+
+        <br /><br />
+
+        <button>Editar campanha</button>
+      </div>
+    </>
+  );
+}
