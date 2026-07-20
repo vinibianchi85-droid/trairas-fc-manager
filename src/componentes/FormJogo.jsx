41b774e478f40f02ba7e3bf5eee7890ad4edaf82
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 13:34:48 -0300

    Update FormJogo.jsx

--- src/componentes/FormJogo.jsx
+++ src/componentes/FormJogo.jsx
@@ -1,1 +1,46 @@
+import { useState } from "react";
 
+export default function FormJogo() {
+  const [adversario, setAdversario] = useState("");
+  const [data, setData] = useState("");
+  const [horario, setHorario] = useState("");
+  const [local, setLocal] = useState("");
+
+  function salvarJogo() {
+    alert("Jogo cadastrado (por enquanto apenas teste).");
+  }
+
+  return (
+    <div className="card">
+      <h2>Novo Jogo</h2>
+
+      <input
+        placeholder="Adversário"
+        value={adversario}
+        onChange={(e) => setAdversario(e.target.value)}
+      />
+
+      <input
+        type="date"
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+      />
+
+      <input
+        type="time"
+        value={horario}
+        onChange={(e) => setHorario(e.target.value)}
+      />
+
+      <input
+        placeholder="Local"
+        value={local}
+        onChange={(e) => setLocal(e.target.value)}
+      />
+
+      <button onClick={salvarJogo}>
+        Salvar jogo
+      </button>
+    </div>
+  );
+}
