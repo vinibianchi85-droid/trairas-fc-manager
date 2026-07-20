7c128a4c5e11461fabc66c2c762190c8d218aa07
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 13:21:53 -0300

    Update Estatísticas.jsx

--- src/pages/Estatísticas.jsx
+++ src/pages/Estatísticas.jsx
@@ -1,1 +1,23 @@
+import jogadores from "../data/jogadores";
 
+export default function Estatisticas() {
+  const artilheiro = [...jogadores].sort((a, b) => b.gols - a.gols)[0];
+
+  return (
+    <>
+      <div className="card">
+        <h2>📊 Estatísticas</h2>
+
+        <p>
+          <strong>Artilheiro:</strong>{" "}
+          {artilheiro ? artilheiro.nome : "Nenhum"}
+        </p>
+
+        <p>
+          <strong>Gols:</strong>{" "}
+          {artilheiro ? artilheiro.gols : 0}
+        </p>
+      </div>
+    </>
+  );
+}
