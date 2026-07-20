52bbc6b9140ba17add927cdc77a5147db5a493bb
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 13:08:05 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import jogadores from "./data/jogadores";
 import { useState } from "react";
 import clube from "./data/clube";
 
@@ -56,8 +57,29 @@ function Jogos() {
 }
 
 function Elenco() {
-  return <div className="card"><h2>Elenco</h2></div>;
+  return (
+    <>
+      <h2>👥 Elenco</h2>
+
+      {jogadores.map((jogador) => (
+        <div className="card" key={jogador.id}>
+          <h3>
+            #{jogador.numero} - {jogador.nome}
+          </h3>
+
+          <p><strong>Posição:</strong> {jogador.posicao}</p>
+          <p>⚽ Gols: {jogador.gols}</p>
+          <p>🎯 Assistências: {jogador.assistencias}</p>
+          <p>🟨 Amarelos: {jogador.amarelos}</p>
+          <p>🟥 Vermelhos: {jogador.vermelhos}</p>
+          <p>🏃 Jogos: {jogador.jogos}</p>
+        </div>
+      ))}
+    </>
+  );
 }
+  
+
 
 function Stats() {
   return <div className="card"><h2>Estatísticas</h2></div>;
