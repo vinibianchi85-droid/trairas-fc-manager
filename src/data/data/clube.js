ced72b5d8aa554fd05faebfd3ffb193a5c04dfe2
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 12:52:49 -0300

    Create clube.js

---
+++ src/data/data/clube.js
@@ -0,0 +1,28 @@
+const clube = {
+  nome: "Traíras FC",
+  temporada: "2026",
+  campeonato: "Citadino",
+
+  proximoJogo: {
+    adversario: "A definir",
+    data: "Em breve",
+    horario: "--:--"
+  },
+
+  ultimoJogo: {
+    adversario: "A definir",
+    placar: "-",
+    resultado: "-"
+  },
+
+  campanha: {
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsPro: 0,
+    golsContra: 0
+  }
+};
+
+export default clube;
