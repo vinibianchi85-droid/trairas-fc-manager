34e72efc616ac4f7cd77bf719ff5c479423207ba
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 21:18:45 -0300

    Update Elenco.jsx

--- src/pages/Elenco.jsx
+++ src/pages/Elenco.jsx
@@ -1,46 +1,99 @@
 import { useState } from "react";
+
 import jogadoresIniciais from "../data/jogadores";
 
 import JogadorForm from "../components/JogadorForm";
 import JogadorCard from "../components/JogadorCard";
 
 export default function Elenco() {
+
   const [jogadores, setJogadores] = useState(jogadoresIniciais);
 
-  function adicionarJogador(jogador) {
+  const [pesquisa, setPesquisa] = useState("");
+
+  function adicionarJogador(jogador){
+
     setJogadores([
       ...jogadores,
       {
         ...jogador,
         id: Date.now(),
-        gols: 0,
-        assistencias: 0,
-        amarelos: 0,
-        vermelhos: 0,
-      },
+        gols:0,
+        assistencias:0,
+        amarelos:0,
+        vermelhos:0,
+        jogos:0
+      }
     ]);
+
   }
 
-  return (
-    <div style={{ padding: 20 }}>
+  const jogadoresFiltrados = jogadores.filter((jogador)=>{
 
-      <h1>Elenco Traíras FC</h1>
+    const texto = pesquisa.toLowerCase();
 
-      <JogadorForm onSalvar={adicionarJogador} />
+    return (
 
-      <hr />
+      jogador.nome.toLowerCase().includes(texto) ||
 
-      {jogadores.length === 0 ? (
-        <p>Nenhum jogador cadastrado.</p>
-      ) : (
-        jogadores.map((jogador) => (
-          <JogadorCard
-            key={jogador.id}
-            jogador={jogador}
-          />
-        ))
-      )}
+      String(jogador.numero).includes(texto)
 
-    </div>
+    );
+
+  });
+
+  return(
+
+<div style={{padding:20}}>
+
+<h1>Elenco Traíras FC</h1>
+
+<JogadorForm onSalvar={adicionarJogador}/>
+
+<hr/>
+
+<input
+
+placeholder="Pesquisar jogador..."
+
+value={pesquisa}
+
+onChange={(e)=>setPesquisa(e.target.value)}
+
+style={{
+width:"100%",
+padding:12,
+marginBottom:20,
+borderRadius:8,
+border:"1px solid #ccc"
+}}
+
+/>
+
+{
+
+jogadoresFiltrados.length===0 ?
+
+<p>Nenhum jogador encontrado.</p>
+
+:
+
+jogadoresFiltrados.map((jogador)=>(
+
+<JogadorCard
+
+key={jogador.id}
+
+jogador={jogador}
+
+/>
+
+))
+
+}
+
+</div>
+
   );
+
 }
