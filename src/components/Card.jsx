e34dd0aba7ae79bcdfb7eeb59c73e4515f69ee39
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 22:00:44 -0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,29 @@
-export default function Card({children}){return <div className='card'>{children}</div>}
+import { colors } from "../styles/theme";
+
+export default function Card({
+  title,
+  children
+}) {
+  return (
+    <div
+      style={{
+        background: colors.card,
+        borderRadius: 16,
+        padding: 20,
+        marginBottom: 20,
+        boxShadow: "0 4px 12px rgba(0,0,0,.12)"
+      }}
+    >
+      <h3
+        style={{
+          color: colors.primary,
+          marginBottom: 15
+        }}
+      >
+        {title}
+      </h3>
+
+      {children}
+    </div>
+  );
+}
