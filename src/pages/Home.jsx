e9bf857c802aa6deb16f470eea44401af513cc77
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 22:01:12 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,32 @@
-export default function Home(){return <div><h2>Home</h2></div>}
+import Header from "../components/Header";
+
+export default function Home() {
+
+  return (
+
+    <>
+
+      <Header />
+
+      <div
+        style={{
+          padding: 25,
+          textAlign: "center"
+        }}
+      >
+
+        <h2>
+          Bem-vindo ao Traíras FC Manager
+        </h2>
+
+        <p>
+          Sistema oficial de gerenciamento do clube.
+        </p>
+
+      </div>
+
+    </>
+
+  );
+
+}
