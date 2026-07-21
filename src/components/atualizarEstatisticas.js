cb11ef48dfd5fe74bfe5e1ac59b4b59b1c1f327d
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 21:28:37 -0300

    Add atualizarEstatisticas function for player stats

---
+++ src/components/atualizarEstatisticas.js
@@ -0,0 +1,26 @@
+export default function atualizarEstatisticas(
+  jogadores,
+  estatisticasPartida
+) {
+  return jogadores.map((jogador) => {
+    const registro = estatisticasPartida.find(
+      (item) => item.jogadorId === jogador.id
+    );
+
+    if (!registro) {
+      return jogador;
+    }
+
+    return {
+      ...jogador,
+      jogos: (jogador.jogos || 0) + 1,
+      gols: (jogador.gols || 0) + registro.gols,
+      assistencias:
+        (jogador.assistencias || 0) + registro.assistencias,
+      amarelos:
+        (jogador.amarelos || 0) + registro.amarelos,
+      vermelhos:
+        (jogador.vermelhos || 0) + registro.vermelhos,
+    };
+  });
+}
