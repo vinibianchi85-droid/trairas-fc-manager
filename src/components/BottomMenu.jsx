e249b76f6704cd2b01202fff56a96725e7c85df4
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 22:31:01 -0300

    Update BottomMenu.jsx

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -7,7 +7,7 @@ import {
   Settings,
 } from "lucide-react";
 
-import { colors } from "../styles/theme";
+import { colors } from "../style/theme";
 
 export default function BottomMenu() {
   const menu = [
