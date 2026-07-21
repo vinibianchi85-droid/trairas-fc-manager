13dd8e07ef5e0b86fcd2168a48642183b69e8ef7
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 21:10:00 -0300

    Update jogadores.js

--- src/data/jogadores.js
+++ src/data/jogadores.js
@@ -1,38 +1,18 @@
 const jogadores = [
   {
     id: 1,
-    nome: "João Alberto",
+    nome: "João Silva",
     numero: 10,
-    posicao: "Atacante",
-    gols: 0,
-    assistencias: 0,
-    amarelos: 0,
-    vermelhos: 0,
-    jogos: 0,
-    foto: ""
-  },
-  {
-    id: 2,
-    nome: "Leonardo Gress",
-    numero: 9,
-    posicao: "Atacante",
-    gols: 0,
-    assistencias: 0,
-    amarelos: 0,
-    vermelhos: 0,
-    jogos: 0,
-    foto: ""
-  },
-  {
-    id: 3,
-    nome: "Lucas Plautz",
-    numero: 7,
     posicao: "Meia",
+    nascimento: "",
+    telefone: "",
+    pe: "Direito",
+    altura: "",
+    peso: "",
     gols: 0,
     assistencias: 0,
     amarelos: 0,
     vermelhos: 0,
-    jogos: 0,
     foto: ""
   }
 ];
