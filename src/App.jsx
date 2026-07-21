79379b3b5b43fea4ff436c443669bbde083f0108
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-07-20 21:02:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,61 @@
-export default function App(){return <h1>Traíras FC Manager</h1>}
+import { useState } from "react";
+
+import Home from "./pages/Home";
+import Jogos from "./pages/Jogos";
+import Elenco from "./pages/Elenco";
+import Campeonato from "./pages/Campeonato";
+import Estatisticas from "./pages/Estatisticas";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
+
+import Header from "./components/Header";
+import BottomMenu from "./components/BottomMenu";
+
+export default function App() {
+  const [pagina, setPagina] = useState("home");
+
+  function renderPagina() {
+    switch (pagina) {
+      case "home":
+        return <Home />;
+
+      case "dashboard":
+        return <Dashboard />;
+
+      case "jogos":
+        return <Jogos />;
+
+      case "elenco":
+        return <Elenco />;
+
+      case "campeonato":
+        return <Campeonato />;
+
+      case "estatisticas":
+        return <Estatisticas />;
+
+      case "admin":
+        return <Admin />;
+
+      default:
+        return <Home />;
+    }
+  }
+
+  return (
+    <div className="app">
+
+      <Header />
+
+      <main>
+        {renderPagina()}
+      </main>
+
+      <BottomMenu
+        pagina={pagina}
+        setPagina={setPagina}
+      />
+
+    </div>
+  );
+}
